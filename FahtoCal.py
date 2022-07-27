af7fb41d74fa716d5a5bba6f7aa3565d60ff0c03
mgassaway1 <103210069+mgassaway1@users.noreply.github.com>
2022-07-27 12:17:31 -0500

    Add files via upload

---
+++ FahtoCal.py
@@ -0,0 +1,11 @@
+print("This is a program that helps convert Fahrenheit to Celsius.")
+
+def main():
+
+        
+   fahrenheit = input("What is the Fahrenheit temperature? ")
+   celsius = 9/5 * fahrenheit - 32
+   print(celsius)
+
+
+main()
